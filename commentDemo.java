954520557bd228facf64892b37d4653e55268180
mingliangZhou518 <3430404814@qq.com>
2026-03-08 14:35:42 +0800

    Add files via upload

---
+++ commentDemo.java
@@ -0,0 +1,20 @@
+package bag;
+
+/**
+ * 程序注释
+ * 这是一个main方法，是程序的入口，必须要有main方法，程序才能执行
+ */
+public class commentDemo
+{
+    public static void main(String[] args) {
+        //目标：认识三种注释的写法
+        //1.单行注释
+        //打印2行诗句
+        System.out.println("hello world");
+
+        /*
+        2.多行注释
+        打印三行诗句，依次打印
+         */
+        System.out.println("hello world");
+    }
